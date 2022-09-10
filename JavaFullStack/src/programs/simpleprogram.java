08e1af177969ddba5d538b048b357375839c06ff
Shashikanth M S <shashikanthms1998@gmail.com>
2022-09-11 00:48:30 +0530

    recent edit shortcuts

--- JavaFullStack/src/programs/simpleprogram.java
+++ JavaFullStack/src/programs/simpleprogram.java
@@ -1,10 +1,24 @@
 package programs;
 
 public class simpleprogram {
+	 int age;
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println("UPPERCSE AND LOWERCASE SYSOUT CONTROOL SPACE CNTRL SHIFT X and cntrl shift y");
 		System.out.println("hello");
+		simpleprogram s=new simpleprogram();
+		s.setAge(10);
 	}
 
 }
