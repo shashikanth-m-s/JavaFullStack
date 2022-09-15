f6d1deab167143c7eedb4541ed6abfe76650f076
Shashikanth M S <shashikanthms1998@gmail.com>
2022-09-15 10:15:44 +0530

    server creation

--- vertex-coreexample/src/main/java/com/smsvertx/app/vertex_coreexample/App.java
+++ vertex-coreexample/src/main/java/com/smsvertx/app/vertex_coreexample/App.java
@@ -1,5 +1,11 @@
 package com.smsvertx.app.vertex_coreexample;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+
 /**
  * Hello world!
  * 
@@ -9,5 +15,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Vertx vrtx=Vertx.vertx();
+        
+        HttpServer server=vrtx.createHttpServer();
+        
+        Router router=Router.router(vrtx);
+        
+        Route hand=router.route().handler(RoutingContext->{
+        	
+        	HttpServerResponse resp=RoutingContext.response();
+        	resp.setChunked(true).write("hello");
+        	
+        });
+        server.requestHandler(router).listen(998);
+        
     }
 }
