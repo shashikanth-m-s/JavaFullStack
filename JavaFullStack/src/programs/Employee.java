ea1e4bf30cbeb96ba9efcb8cbc5b1fa6de9ad565
Shashikanth M S <shashikanthms1998@gmail.com>
2022-09-15 10:03:01 +0530

    emplooyee class details

---
+++ JavaFullStack/src/programs/Employee.java
@@ -0,0 +1,30 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		String Ename;
+		int Eage;
+		float Esalary;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name of Employee");
+		Ename=sc.nextLine();
+		System.out.println("Enter age Employee");
+		Eage=sc.nextInt();
+		System.out.println("Enter salary Employee");
+		Esalary=sc.nextFloat();
+		
+		System.out.println("Employee Details Are");
+		System.out.println( "name="+Ename);
+		System.out.println("age="+Eage);
+		System.out.println("salary="+Esalary);
+		
+		
+		
+
+	}
+
+}
